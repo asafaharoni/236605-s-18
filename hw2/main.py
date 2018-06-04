7a7785b2f017a9cf2478e79ae471c5b525edb225
daniel-yakubov <37901932+daniel-yakubov@users.noreply.github.com>
2018-06-04 15:01:15 +0300

    Create main.py

---
+++ hw2/main.py
@@ -0,0 +1,236 @@
+import  torch
+import  torch.nn  as nn
+from torch.nn import init
+from torch.autograd import Variable
+import torchvision
+import torchvision.transforms as T
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+import torchvision.datasets as dset
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+def show_images(images):
+    images = np.reshape(images, [images.shape[0], -1])  # images reshape to (batch_size, D)
+    sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
+    sqrtimg = int(np.ceil(np.sqrt(images.shape[1])))
+
+    fig = plt.figure(figsize=(sqrtn, sqrtn))
+    gs = gridspec.GridSpec(sqrtn, sqrtn)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, img in enumerate(images):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(img.reshape([sqrtimg,sqrtimg]))
+    return 
+
+def preprocess_img(x):
+    return 2 * x - 1.0
+
+def deprocess_img(x):
+    return (x + 1.0) / 2.0
+
+def rel_error(x,y):
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+def count_params(model):
+    """Count the number of parameters in the current PyTorch graph """
+    param_count = np.sum([np.prod(p.size()) for p in model.parameters()])
+    return param_count
+
+answers = np.load('gan-checks-tf.npz')
+
+class ChunkSampler(sampler.Sampler):
+    """Samples elements sequentially from some offset. 
+    Arguments:
+        num_samples: # of desired datapoints
+        start: offset where we should start selecting from
+    """
+    def __init__(self, num_samples, start=0):
+        self.num_samples = num_samples
+        self.start = start
+
+    def __iter__(self):
+        return iter(range(self.start, self.start + self.num_samples))
+
+    def __len__(self):
+        return self.num_samples
+
+NUM_TRAIN = 50000
+NUM_VAL = 5000
+
+NOISE_DIM = 96
+batch_size = 128
+
+mnist_train = dset.MNIST('./cs231n/datasets/MNIST_data', train=True, download=True,
+                           transform=T.ToTensor())
+loader_train = DataLoader(mnist_train, batch_size=batch_size,
+                          sampler=ChunkSampler(NUM_TRAIN, 0))
+
+mnist_val = dset.MNIST('./cs231n/datasets/MNIST_data', train=True, download=True,
+                           transform=T.ToTensor())
+loader_val = DataLoader(mnist_val, batch_size=batch_size,
+                        sampler=ChunkSampler(NUM_VAL, NUM_TRAIN))
+
+
+imgs = loader_train.__iter__().next()[0].view(batch_size, 784).numpy().squeeze()
+show_images(imgs)
+
+def sample_noise(batch_size, dim):
+    return (2 * torch.rand(batch_size, dim)) - 1
+
+def test_sample_noise():
+    batch_size = 3
+    dim = 4
+    torch.manual_seed(231)
+    z = sample_noise(batch_size, dim)
+    np_z = z.cpu().numpy()
+    assert np_z.shape == (batch_size, dim)
+    assert torch.is_tensor(z)
+    assert np.all(np_z >= -1.0) and np.all(np_z <= 1.0)
+    assert np.any(np_z < 0.0) and np.any(np_z > 0.0)
+    print('All tests passed!')
+    
+test_sample_noise()
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        N, C, H, W = x.size() # read in N, C, H, W
+        return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+    
+class Unflatten(nn.Module):
+    """
+    An Unflatten module receives an input of shape (N, C*H*W) and reshapes it
+    to produce an output of shape (N, C, H, W).
+    """
+    def __init__(self, N=-1, C=128, H=7, W=7):
+        super(Unflatten, self).__init__()
+        self.N = N
+        self.C = C
+        self.H = H
+        self.W = W
+    def forward(self, x):
+        return x.view(self.N, self.C, self.H, self.W)
+
+def initialize_weights(m):
+    if isinstance(m, nn.Linear) or isinstance(m, nn.ConvTranspose2d):
+        init.xavier_uniform_(m.weight.data)
+
+dtype = torch.FloatTensor
+
+def discriminator():
+    """
+    Build and return a PyTorch model implementing the architecture above.
+    """
+    model = nn.Sequential(
+        nn.Linear(784, 256, bias=True),
+        nn.LeakyReLU(0.1),
+        nn.Linear(256, 256, bias=True),
+        nn.LeakyReLU(0.1),
+        nn.Linear(256, 1, bias=True),
+    )
+    return model
+
+def test_discriminator(true_count=267009):
+    model = discriminator()
+    cur_count = count_params(model)
+    if cur_count != true_count:
+        print('Incorrect number of parameters in discriminator. Check your achitecture.')
+    else:
+        print('Correct number of parameters in discriminator.')     
+
+test_discriminator()
+
+def generator(noise_dim=NOISE_DIM):
+    """
+    Build and return a PyTorch model implementing the architecture above.
+    """
+    model = nn.Sequential(
+        nn.Linear(noise_dim, 1024, bias=True),
+        nn.ReLU(),
+        nn.Linear(1024, 1024, bias=True),
+        nn.ReLU(),
+        nn.Linear(1024, 784, bias=True),
+        nn.Tanh()
+    )
+    return model
+
+
+def test_generator(true_count=1858320):
+    model = generator(4)
+    cur_count = count_params(model)
+    if cur_count != true_count:
+        print('Incorrect number of parameters in generator. Check your achitecture.')
+    else:
+        print('Correct number of parameters in generator.')
+
+test_generator()
+
+def bce_loss(input, target):
+    neg_abs = - input.abs()
+    loss = input.clamp(min=0) - input * target + (1 + neg_abs.exp()).log()
+    return loss.mean()
+
+def discriminator_loss(logits_real, logits_fake):
+    """
+    Computes the discriminator loss described above.
+    
+    Inputs:
+    - logits_real: PyTorch Tensor of shape (N,) giving scores for the real data.
+    - logits_fake: PyTorch Tensor of shape (N,) giving scores for the fake data.
+    
+    Returns:
+    - loss: PyTorch Tensor containing (scalar) the loss for the discriminator.
+    """
+
+    real_labels = torch.ones(logits_real.size(0),logits_real.size(1)).type(dtype)
+    real_loss = bce_loss(logits_real,real_labels)
+
+    fake_labels = torch.zeros(logits_fake.size(0),logits_fake.size(1)).type(dtype)
+    fake_loss = bce_loss(logits_fake,fake_labels)
+
+    loss = fake_loss + real_loss
+    
+    return loss
+
+def generator_loss(logits_fake):
+    """
+    Computes the generator loss described above.
+
+    Inputs:
+    - logits_fake: PyTorch Tensor of shape (N,) giving scores for the fake data.
+    
+    Returns:
+    - loss: PyTorch Tensor containing the (scalar) loss for the generator.
+    """
+    
+    fake_labels = torch.ones(logits_fake.size(0),logits_fake.size(1)).type(dtype)
+    loss = bce_loss(logits_fake,fake_labels)
+    
+    return loss
+
+def test_discriminator_loss(logits_real, logits_fake, d_loss_true):
+    d_loss = discriminator_loss(torch.Tensor(logits_real).type(dtype), torch.Tensor(logits_fake).type(dtype)).cpu().numpy()
+    print("Maximum error in d_loss: %g"%rel_error(d_loss_true, d_loss))
+
+test_discriminator_loss(answers['logits_real'], answers['logits_fake'],
+                        answers['d_loss_true'])
+
+def test_generator_loss(logits_fake, g_loss_true):
+    g_loss = generator_loss(torch.Tensor(logits_fake).type(dtype)).cpu().numpy()
+    print("Maximum error in g_loss: %g"%rel_error(g_loss_true, g_loss))
+
+test_generator_loss(answers['logits_fake'], answers['g_loss_true'])
+
