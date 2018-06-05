65b0da8fa3ab44f459b5b2a8b98712a84816d8fe
daniel-yakubov <37901932+daniel-yakubov@users.noreply.github.com>
2018-06-05 11:11:19 +0300

    Update main.py

--- hw2/main.py
+++ hw2/main.py
@@ -129,12 +129,12 @@ def initialize_weights(m):
         init.xavier_uniform_(m.weight.data)
 
 dtype = torch.FloatTensor
-
 def discriminator():
     """
     Build and return a PyTorch model implementing the architecture above.
     """
     model = nn.Sequential(
+        Flatten(),
         nn.Linear(784, 256, bias=True),
         nn.LeakyReLU(0.1),
         nn.Linear(256, 256, bias=True),
@@ -202,7 +202,7 @@ def discriminator_loss(logits_real, logits_fake):
     fake_loss = bce_loss(logits_fake,fake_labels)
 
     loss = fake_loss + real_loss
-    print loss
+    
     return loss
 
 def generator_loss(logits_fake):
@@ -233,4 +233,70 @@ def test_generator_loss(logits_fake, g_loss_true):
     print("Maximum error in g_loss: %g"%rel_error(g_loss_true, g_loss))
 
 test_generator_loss(answers['logits_fake'], answers['g_loss_true'])
+
+def get_optimizer(model):
+    optimizer = optim.Adam(model.parameters(), lr = 0.001 , betas=(0.5, 0.999))
+    return optimizer
+
+def run_a_gan(D, G, D_solver, G_solver, discriminator_loss, generator_loss, show_every=250, 
+              batch_size=128, noise_size=96, num_epochs=10):
+    """
+    Train a GAN!
+    
+    Inputs:
+    - D, G: PyTorch models for the discriminator and generator
+    - D_solver, G_solver: torch.optim Optimizers to use for training the
+      discriminator and generator.
+    - discriminator_loss, generator_loss: Functions to use for computing the generator and
+      discriminator loss, respectively.
+    - show_every: Show samples after every show_every iterations.
+    - batch_size: Batch size to use for training.
+    - noise_size: Dimension of the noise to use as input to the generator.
+    - num_epochs: Number of epochs over the training dataset to use for training.
+    """
+    iter_count = 0
+    for epoch in range(num_epochs):
+        for x, _ in loader_train:
+            if len(x) != batch_size:
+                continue
+            D_solver.zero_grad()
+            real_data = x.type(dtype)
+            logits_real = D(2* (real_data - 0.5)).type(dtype)
+
+            g_fake_seed = sample_noise(batch_size, noise_size).type(dtype)
+            fake_images = G(g_fake_seed).detach()
+            logits_fake = D(fake_images.view(batch_size, 1, 28, 28))
+
+            d_total_error = discriminator_loss(logits_real, logits_fake)
+            d_total_error.backward()        
+            D_solver.step()
+
+            G_solver.zero_grad()
+            g_fake_seed = sample_noise(batch_size, noise_size).type(dtype)
+            fake_images = G(g_fake_seed)
+
+            gen_logits_fake = D(fake_images.view(batch_size, 1, 28, 28))
+            g_error = generator_loss(gen_logits_fake)
+            g_error.backward()
+            G_solver.step()
+
+            if (iter_count % show_every == 0):
+                print('Iter: {}, D: {:.4}, G:{:.4}'.format(iter_count,d_total_error.item(),g_error.item()))
+                imgs_numpy = fake_images.data.cpu().numpy()
+                show_images(imgs_numpy[0:16])
+                plt.show()
+                print()
+            iter_count += 1
+
+# Make the discriminator
+D = discriminator().type(dtype)
+
+# Make the generator
+G = generator().type(dtype)
+
+# Use the function you wrote earlier to get optimizers for the Discriminator and the Generator
+D_solver = get_optimizer(D)
+G_solver = get_optimizer(G)
+# Run it!
+run_a_gan(D, G, D_solver, G_solver, discriminator_loss, generator_loss)
 
